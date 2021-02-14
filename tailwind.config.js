d778137722a219e3622df342f55a0026fc07d43e
LeonardoLeonardi <leonardoleonardi@keix.com>
2021-02-15 00:17:42 +0100

    Bug fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 // tailwind.config.js
 module.exports = {
-  purge: [],
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
+    colors: { dark: { 1: "#121212" } },
     extend: {},
   },
   variants: {
