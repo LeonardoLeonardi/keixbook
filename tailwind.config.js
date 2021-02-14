fbb4a7005ede99aaeeedb75fb881aa4c95a2ea0e
LeonardoLeonardi <leonardoleonardi@keix.com>
2021-02-15 00:27:20 +0100

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,7 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
-    colors: { dark: { 1: "#121212" } },
-    extend: {},
+    extend: { colors: { darkM: { 1: "#121212" } } },
   },
   variants: {
     extend: {},
