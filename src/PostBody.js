b823723c98f9ce42e08b525ac3b86dea8a771e31
LeonardoLeonardi <leonardoleonardi@keix.com>
2021-02-15 08:47:46 +0100

    Overflow

--- src/PostBody.js
+++ src/PostBody.js
@@ -4,7 +4,9 @@ function PostBody(props) {
   return (
     <div className="bg-white  max-w-md  ml-5 dark:bg-darkM-1 ">
       <div className="ml-2 mr-1">
-        <p className="text-gray-900 text-base dark:text-white">{props.testo}</p>
+        <p className="text-gray-900 text-base dark:text-white overflow-auto">
+          {props.testo}
+        </p>
       </div>
     </div>
   );
